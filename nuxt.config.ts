f7148c5eab39a747ce0466ecb254d08adfa68e6f
Simón Oroño <simonorono@protonmail.com>
2023-11-03 22:14:14 -0400

    Add Inter font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
+  css: [
+    '~/assets/css/main.css',
+    '~/node_modules/@fontsource-variable/inter/index.css',
+  ],
   devtools: { enabled: true },
   modules: ['nuxt-simple-sitemap'],
   postcss: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     './error.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter Variable', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
