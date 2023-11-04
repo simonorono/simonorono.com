a95c501f0b2398f1d13b8cfbb4f4cdccac5f3a0c
Simón Oroño <simonorono@protonmail.com>
2023-11-04 13:33:53 -0400

    Add icons and other static files

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,42 @@
 const route = useRoute()
 useHead({
   title: title(),
-  link: [{ rel: 'canonical', href: canonical(route.path) }],
+  link: [
+    { rel: 'canonical', href: canonical(route.path) },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'mask-icon',
+      color: '#5bbad5',
+      href: '/safari-pinned-tab.svg',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+  ],
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'msapplication-TileColor', content: '#da532c' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'theme-color', content: '#ffffff' },
+  ],
 })
 </script>
 
