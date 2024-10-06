c26ee16b134038608a26666737cdc789145d7b92
Simón Oroño <simonorono@protonmail.com>
2024-10-06 17:25:35 -0400

    Update dependencies and format project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    '~/assets/css/main.css',
-  ],
+  css: ['~/assets/css/main.css'],
 
   devtools: { enabled: true },
   modules: ['nuxt-simple-sitemap'],
