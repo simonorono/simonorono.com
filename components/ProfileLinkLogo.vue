daa1ea2a63aebf577ca35c0960a9c8545d2738b0
Simón Oroño <simonorono@protonmail.com>
2024-11-29 09:22:10 -0400

    Add rel="me" to profile links

--- components/ProfileLinkLogo.vue
+++ components/ProfileLinkLogo.vue
@@ -7,7 +7,7 @@ const { href, name, src } = defineProps<{
 </script>
 
 <template>
-  <NuxtLink :href="href" class="inline-block" target="_blank">
+  <NuxtLink :href="href" class="inline-block" target="_blank" rel="me">
     <img :alt="`${name} logo`" class="h-10 w-10" :src="src" />
     <span class="sr-only">{{ name }} profile (opens in new tab)</span>
   </NuxtLink>
