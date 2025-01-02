9c46aa0b6d959899aa7419d822aa84f945643a39
Simón Oroño <simonorono@protonmail.com>
2025-01-01 21:30:20 -0400

    Simplify back to index link

--- components/BackToIndex.vue
+++ components/BackToIndex.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-const { classes } = defineProps<{ classes?: string }>()
-</script>
-
 <template>
-  <NuxtLink
-    to="/"
-    :class="['group inline-block', classes].filter(Boolean).join(' ')"
-  >
-    ←
-    <span
-      class="cursor-pointer underline sm:no-underline sm:group-hover:underline"
-    >
-      back to index
-    </span>
+  <NuxtLink to="/" class="link">
+    ← back to index
   </NuxtLink>
 </template>
